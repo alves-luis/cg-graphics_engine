6ecba807b4d8d9ff966098624f6cafd0b8560a11
Luís Alves <luismig.alves@gmail.com>
2019-02-22 22:58:04 +0000

    Minor changes to parsing arguments

--- generator/Generator.cpp
+++ generator/Generator.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+// Helper struct to parse arguments
+typedef struct args {
+    float floats[3]; // max number of floats as args
+    int ints[2]; // max number of ints as args
+} * Arguments;
+
 // Sphere uses 3 args (radius, slices and stacks)
 const int SPHERE_ARGS = 3;
 
@@ -32,21 +38,30 @@ void createSphere(float rad, int slices, int stacks, string fname) {
 }
 
 void createCone(float botRad, float height, int slices, int stacks, string fname) {
+  cout << botRad << "," << height << "," << slices << "," << stacks << "," << fname;
 
 }
 
 /**
- * Helper function to put args into int. Returns NULL if error.
+ * Helper function to put args into proper types. Returns NULL if error.
  * */
-float * argsUpdate(char**argv, int max) {
-  float * result = (float *) malloc(sizeof(float)*max);
+Arguments argsUpdate(char**argv, int numFloat, int numInt) {
+  Arguments result = (Arguments) malloc(sizeof(struct args));
 
   if (!result)
     return result;
 
-  for(int i = 0; i < max; i++)
-    if (argv[i+2])
-      result[i] = stof(argv[i+2]);
+  for(int i = 0; i < numFloat; i++)
+    if (argv[i])
+      result->floats[i] = stof(argv[i]);
+    else
+      return NULL;
+
+  argv += numFloat;
+
+  for(int i = 0; i < numInt; i++)
+    if (argv[i])
+      result->ints[i] = stoi(argv[i]);
     else
       return NULL;
 
@@ -71,12 +86,12 @@ int main(int argc, char** argv) {
       return 1;
     }
 
-    float * args = argsUpdate(argv,CONE_ARGS);
+    Arguments args = argsUpdate(argv+2,2,2);
     if (!args) {
       cout << "Invalid arguments!";
       return 1;
     }
-    createCone(args[0],args[1], static_cast<int>(args[2]), static_cast<int>(args[3]),argv[2+CONE_ARGS]);
+    createCone(args->floats[0],args->floats[1],args->ints[0],args->ints[1],argv[2+CONE_ARGS]);
     free(args);
   }
   else if (prim == "sphere") {
@@ -86,12 +101,12 @@ int main(int argc, char** argv) {
       return 1;
     }
 
-    float * args = argsUpdate(argv,SPHERE_ARGS);
+    Arguments args = argsUpdate(argv+2,1,2);
     if (!args) {
       cout << "Invalid arguments!";
       return 1;
     }
-    createSphere(args[0], static_cast<int>(args[1]), static_cast<int>(args[2]),argv[2+SPHERE_ARGS]);
+    createSphere(args->floats[0], args->ints[0], args->ints[1],argv[2+SPHERE_ARGS]);
     free(args);
   }
   else if (prim == "box") {
@@ -101,13 +116,13 @@ int main(int argc, char** argv) {
       return 1;
     }
 
-    float * args;
+    Arguments args;
     bool divisions = false;
     if (argc == 2 + BOX_ARGS) { // if no number of divisions
-      args = argsUpdate(argv, BOX_ARGS);
+      args = argsUpdate(argv+2, 3, 0);
     }
     else if (argc == 3 + BOX_ARGS) { // if number of divisions specified
-      args = argsUpdate(argv, BOX_ARGS + 1);
+      args = argsUpdate(argv+2,3,1);
       divisions = true;
     }
 
@@ -117,9 +132,9 @@ int main(int argc, char** argv) {
     }
 
     if (divisions)
-      createBox(args[0],args[1],args[2],args[3],argv[3+BOX_ARGS]);
+      createBox(args->floats[0],args->floats[1],args->floats[2],args->ints[0],argv[3+BOX_ARGS]);
     else
-      createBox(args[0],args[1],args[2],0,argv[2+BOX_ARGS]);
+      createBox(args->floats[0],args->floats[1],args->floats[2],0,argv[2+BOX_ARGS]);
 
     free(args);
   }
