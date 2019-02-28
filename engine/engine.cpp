bb577bba8a4bac71948e069be345b545188d1bc7
Luís Alves <luismig.alves@gmail.com>
2019-02-28 16:22:13 +0000

    Can parse .3d files

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -1,15 +1,41 @@
 #include "tinyxml/tinyxml2.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
+#include <vector>
 
 using namespace tinyxml2;
-using namespace std;
 
-typedef struct models {
+typedef struct vertex {
+  float x;
+  float y;
+  float z;
+} * Vertex;
 
-};
+std::vector<Vertex> vertexes;
 
-int parse3D(const char * fname) {
-  return 1;
+int parse3D(char * fname) {
+  FILE * file = fopen(fname,"r");
+
+  if (file == NULL)
+    return 1;
+
+  float x, y, z;
+  char * line = (char *) malloc(sizeof(char)*1024);
+  int read;
+  size_t size;
+  while((read = getline(&line, &size, file)) > 0) {
+    int suc = sscanf(line,"%f %f %f",&x,&y,&z);
+    if (suc != 3)
+      return 1;
+    Vertex v = (Vertex) malloc(sizeof(struct vertex));
+    v->x = x;
+    v->y = y;
+    v->z = z;
+    vertexes.push_back(v);
+  }
+
+  return 0;
 }
 
 int loadXML(char * fname) {
@@ -24,10 +50,10 @@ int loadXML(char * fname) {
     return XML_ERROR_PARSING_ELEMENT;
 
   XMLElement* model = scene->FirstChildElement("model");
+  int error;
   while(model != NULL) {
-    const char * fileName = model->Attribute("file");
-    cout << fileName << "\n";
-    int error = parse3D(fileName);
+    char * fileName = (char *) model->Attribute("file");
+    error = parse3D(fileName);
     model = model->NextSiblingElement("model");
   }
 }
@@ -35,10 +61,9 @@ int loadXML(char * fname) {
 int main(int argc, char** argv) {
 
   if (argc < 2) {
-    cout << "Invalid configuration file!\n";
+    std::cout << "Invalid configuration file!\n";
     return 1;
   }
 
   int error = loadXML(argv[1]);
-  cout << error;
 }
