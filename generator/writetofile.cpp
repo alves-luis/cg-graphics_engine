2c62fa1b94a32502bb5ecd04bb2335ed3a3bf98f
Rafaela Rodrigues <rafaelaricorodrigues@gmail.com>
2019-03-01 23:23:49 +0000

    Escrita para ficheiro pronta

--- generator/writetofile.cpp
+++ generator/writetofile.cpp
@@ -2,6 +2,15 @@
 #include <stdio.h>
 
 int writeToFile(float x, float y, float z, char * fName) {
-  printf("%f %f %f\n",x,y,z);
+  FILE * file;
+
+  file=fopen(fName,"ab+");
+  if(file == NULL)
+  {
+    printf("Error!");
+    exit(1);
+  }
+  fprintf(file, "%f %f %f\n",x,y,z);
+  fclose(file);
   return 1;
 }
